25307e58fa67a652c94c9723800d55e8800a1cd9
gevanidar <alissongevanidar@proton.me>
2024-12-08 21:52:55 +0100

    (Add): advent of code question 7 b

---
+++ advent-of-code/2024/7/b/q.py
@@ -0,0 +1,73 @@
+import re
+from itertools import product
+
+
+def getLines(file_name):
+    lines = []
+    with open(file_name, 'r') as f:
+        lines = [l.strip() for l in f.readlines()]
+    return lines
+
+def process_lines(lines):
+    answers = []
+    equations = []
+    for line in lines:
+        y, xs = line.split(":")
+        answers.append(int(y.strip()))
+        equation = []
+        xs = xs.split(" ")
+        for x in xs:
+            if x:
+                equation.append(int(x))
+        equations.append(equation)
+
+    return answers, equations
+
+
+
+
+def checkValid(answer, equation, muls):
+    #print(equation, muls)
+    for mul in muls:
+        val = equation[0]
+        tmp = str(val)#print(val)
+        last_eq = ''
+        for index, eq in enumerate(equation[1:]):
+            if mul[index] == "*":
+                val *= eq
+                tmp += f'*{eq}'
+            elif mul[index] == "+":
+                val += eq
+                tmp += f'+{eq}'
+            else:
+                #print(f'{val} || {eq}')
+                val = int(str(val)+ str(eq))
+                #print(val)
+        #print(f'{tmp=} ?= {answer=}, {val=}')
+        if val == answer:
+            return True
+
+    return False
+
+
+def solve(file_name):
+    lines = getLines(file_name)
+    answers, equations = process_lines(lines)
+
+    total = 0
+    for answer, equation in zip(answers, equations):
+        #print('------------------------------------')
+        operators = ["+", "*", "|"]
+        muls = [''.join(s) for s in product(operators, repeat=len(equation))]
+        #print(muls)
+
+        valid = checkValid(answer, equation, muls)
+        if valid:
+            print(f'{answer}, {equation=}')
+            total += answer
+
+    return total
+
+if __name__ == "__main__":
+    count = solve("input_b.txt")
+    print(count)
